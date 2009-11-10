6143add0ded69ec469c2c37840f86d97e0490eb6
Brandon Kelly <me@brandon-kelly.com>
2009-11-09 16:46:27 -0800

    first commit

---
+++ pi.numsnob.php
@@ -0,0 +1,32 @@
+<?php
+
+$plugin_info = array(
+	'pi_name'			=> 'Number Snob',
+	'pi_version'		=> '1.0',
+	'pi_author'			=> 'Brandon Kelly',
+	'pi_author_url'		=> 'http://brandon-kelly.com/'
+);
+
+class Numsnob {
+
+	var $return_data;
+
+	function Numsnob()
+	{
+		global $TMPL;
+
+		// get the number
+		$num = $TMPL->fetch_param('num');
+
+		// make number a word?
+		$map = array('0', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine');
+		if (isset($map[$num])) $num = $map[$num];
+
+		// capitalize first letter?
+		if ($TMPL->fetch_param('ucfirst') == 'yes') $num = ucfirst($num);
+
+		// "return"
+		$this->return_data = $num;
+	}
+
+}
